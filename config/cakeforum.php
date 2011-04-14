edeb83f0db078d30259a54c2f3e69a0a24339980
choirul <culfianto@xintesa.com>
2011-04-15 03:48:18 +0700

    add config cake forum

---
+++ config/cakeforum.php
@@ -0,0 +1,11 @@
+<?php
+//configure field for table users
+Configure::write('username', 'username');
+#id, username, email, created
+Configure::write('fieldUser', 'User.id, User.username, User.email, User.created');
+#id, username
+Configure::write('searchUser', 'User.id, User.username');
+Configure::write('namefieldUserId', 'id');
+
+?>
+
